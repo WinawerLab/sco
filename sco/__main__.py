d25d3f97f6d497df950e414c6fb70c52fe3a8b82
noahbenson <nben@nyu.edu>
2016-08-31 13:39:10 -0400

    Main now supports tarballs (does not clean them up)

--- sco/__main__.py
+++ sco/__main__.py
@@ -3,7 +3,7 @@
 # The main function, if sco is invoked directly as command.
 # By Noah C. Benson
 
-import os, sys, math
+import os, sys, math, tempfile, tarfile
 import pysistence
 
 from neuropythy.util import CommandLineParser
@@ -14,6 +14,36 @@ main_parser = CommandLineParser(
      ('e', 'max-eccen', 'max_eccentricity',  '20'),
      ('o', 'output',    'output_dir',        '.')])
 
+def _check_extract(arg, subq=False):
+    img_formats = ['.png', '.jpg', '.gif']
+    if len(arg) > 7 and arg[-7:] == '.tar.gz' or len(f) > 4 and f[-4:] == '.tgz':
+        tdir = tempfile.mkdtemp()
+        tf = tarfile.TarFile(name=arg, mode='r')
+        tf.extractall(path=tdir)
+        l = [f for f in os.listdir(tdir) if f[0] != '.']
+        if subq:
+            # Return just the subject path
+            if 'surf' in l and 'mri' in l: return tdir
+            elif len(l) == 0:
+                raise ValueError('Extracted tar-file but found no subject directory!')
+            else:
+                return os.path.join(tdir, l[0])
+        else:
+            # Return a list of image paths
+            return [fl
+                    for fl0 in l
+                    for fl in ([l] if os.path.isfile(l) else os.listdir(l))
+                    if len(l) > 4 and fl[-4:].lower() in img_formats]
+    elif subq:
+        return arg
+    else:
+        if os.path.isdir(arg):
+            return [ff
+                    for ff in os.listdir(arg)
+                    if ff[-4:].lower() in img_formats]
+        else:
+            return [arg] if len(arg) > 4 and arg[-4:].lower() in img_formats
+
 def main(argv):
     (args, opts) = main_parser(argv)
     if len(args) < 2: raise ValueError('Syntax: sco <subject> <images...>')
@@ -21,14 +51,9 @@ def main(argv):
     opts['max_eccentricity']  = float(opts['max_eccentricity'])
     # Arg 0 is a subject; arg 1 is a directory of images
     imfiles = []
-    for f in argv[1:]:
-        if os.path.isdir(f):
-            for ff in os.listdir(f):
-                if ff[-4:] in ['.png', '.jpg', '.gif']:
-                    imfiles.append(ff)
-        else:
-            imfiles.append(ff)
-    r = calc_sco(opts, subject=args[0], stimulus_image_filenames=imfiles)
+    sub = _check_extract(arg[0], True)
+    args = [a for arg in args[1:] for a in _check_extract(arg)]
+    r = calc_sco(opts, subject=sub, stimulus_image_filenames=imfiles)
     export_predicted_responses(r, export_path=opts['output_dir'])
     return 0
 

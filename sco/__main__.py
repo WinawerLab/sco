21b2d3606e2955d19d9e05b3a77ece761c527669
noahbenson <nben@nyu.edu>
2016-08-31 13:45:42 -0400

    added help

--- sco/__main__.py
+++ sco/__main__.py
@@ -10,12 +10,30 @@ from neuropythy.util import CommandLineParser
 from sco import (calc_sco, export_predicted_responses)
 
 main_parser = CommandLineParser(
-    [('p', 'deg2px',    'pixels_per_degree', '24'),
+    [('h', 'help',      'help',              False),
+     ('p', 'deg2px',    'pixels_per_degree', '24'),
      ('e', 'max-eccen', 'max_eccentricity',  '20'),
      ('o', 'output',    'output_dir',        '.')])
 
+sco_help = \
+'''
+Usage: sco <subject> <image0000> <image0001>...
+Runs the Standard Cortical Observer prediction routine and exports a series of
+MGZ files: prediction_0000.mgz, prediction_0001.mgz, etc.
+The following options may be given:
+  * -h|--help prints this message.
+  * -p|--deg2px=<value> sets the pixels per degree in the input images
+    (default: 24).
+  * -e|--max-eccen=<value> sets the maximum eccentricity modeled in the output
+    (default: 20).
+  * -o|--output=<directory> sets the output directory (default: .).
+'''
+
 def main(argv):
     (args, opts) = main_parser(argv)
+    if opts['help']:
+        print sco_help
+        return 1
     if len(args) < 2: raise ValueError('Syntax: sco <subject> <images...>')
     opts['pixels_per_degree'] = float(opts['pixels_per_degree'])
     opts['max_eccentricity']  = float(opts['max_eccentricity'])

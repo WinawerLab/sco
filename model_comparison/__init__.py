c85d67fa50d1daea73d761f7d2c4e5862fa121b0
William F. Broderick <billbrod@gmail.com>
2016-10-04 14:57:20 -0400

    Adds model_comparison/__init__.py

---
+++ model_comparison/__init__.py
@@ -0,0 +1,11 @@
+####################################################################################################
+# model_comparison/__init__.py
+# The model comparison module of the standard cortical observer
+# by William F. Broderick
+
+"""
+
+"""
+import core
+from .core import create_model_dataframe
+import compare_with_Kay2013

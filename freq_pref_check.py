be4b7c93984dbcd1d06b0ef99c7eb4c323280e9a
winawerlab <billbrod@gmail.com>
2016-11-10 17:49:45 -0500

    compare_with_Kay2013 now uses **kwargs
    With this, the compare_with_Kay2013 function now uses **kwargs to allow
    the setting of some of the parameters from an outside call. The function
    will set defaults if some keys are missing from kwargs, but it won't
    overwrite them. I need to check if this is the correct way to do it, but
    this makes it more flexible.
    
    Also, small changes to freq_pref_check:
    
     - filenames now use %02d formatting (so they sort the way we want)
    
     - now uses 1 cpd instead of 3
    
     - checks 1 through 21 cycles per image
    
     - renames frequency to cycles per image in plot_df
    
     - returns plot_df

--- freq_pref_check.py
+++ freq_pref_check.py
@@ -7,6 +7,7 @@
 
 import re
 import os
+import shutil
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,7 +18,7 @@ def generate_stimuli(img_folder, freqs):
         img_folder += '/'
     if not os.path.isdir(img_folder):
         os.makedirs(img_folder)
-    img_name = "freq_{}_grating.png"
+    img_name = "freq_{:02d}_grating.png"
     x = np.asarray(range(1000))/1000.0
     x = np.meshgrid(x,x)[0]
     for fr in freqs:
@@ -27,16 +28,24 @@ def generate_stimuli(img_folder, freqs):
 def main(model_df_path="./sco_freq_prefs.csv", subject='test-sub', subject_dir=None,
          img_folder="~/Desktop/freq_pref_imgs"):
     img_folder = os.path.expanduser(img_folder)
-    freqs = np.asarray(range(5,65,5))
+    shutil.rmtree(img_folder)
+    freqs = np.asarray(range(20))
     generate_stimuli(img_folder, freqs+1)
-    results, stimulus_model_names = compare_with_Kay2013(img_folder, range(len(freqs)), range(3), subject,
-                                                         subject_dir)
+    def freq_pref(e, s, l):
+        # This takes in the eccentricity, size, and area, but we don't use any of them, since we
+        # just want to use 1 cpd (for testing) and ignore everything else. And this must be floats.
+        return {1.0: 1.0}
+    kwargs = {'pRF_frequency_preference_function': freq_pref}
+    results, stimulus_model_names = compare_with_Kay2013(img_folder, range(len(freqs)), range(3),
+                                                         subject, subject_dir,
+                                                         pRF_frequency_preference_function=freq_pref,
+                                                         stimulus_pixels_per_degree=212)
     model_df = create_model_dataframe(results, stimulus_model_names, model_df_path)
 
     plot_df = _create_plot_df(freqs, model_df)
     plot_df = plot_df[plot_df.language == 'python']
-    plot_df['frequency'] = plot_df['image'].apply(lambda x: int(re.search(r'freq_([0-9]+)', x).groups()[0]))
+    plot_df['cycles_per_image'] = plot_df['image'].apply(lambda x: int(re.search(r'freq_([0-9]+)', x).groups()[0]))
 
-    g = sns.factorplot(data=plot_df, x='frequency', y='predicted_responses')
-    
-    return model_df, results
+    g = sns.factorplot(data=plot_df, x='cycles_per_image', y='predicted_responses')
+
+    return model_df, results, plot_df

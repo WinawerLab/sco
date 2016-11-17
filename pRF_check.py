601127b02e313d8fa86ea4bff560acee894d7494
winawerlab <billbrod@gmail.com>
2016-11-17 18:14:49 -0500

    pRF_check can generate stimuli from command line

--- pRF_check.py
+++ pRF_check.py
@@ -4,6 +4,7 @@
 #
 # by William F. Broderick
 
+import argparse
 import scipy as sp
 import numpy as np
 import sco
@@ -20,8 +21,19 @@ def create_stimuli(stimuli_path, save_template_string):
         new_stimuli['vert_sweep'].append((tmp+.5)*255.)
     new_stimuli['horiz_sweep'].append(stimuli[99])
     new_stimuli['vert_sweep'].append(stimuli[130])
-    for name in ['vert_sweep', 'horiz_sweep']:
-        sp.io.savemat(save_template_string.format(name),
-                      {'images': np.asarray(new_stimuli[name])})
+    new_stimuli['horiz_sweep'].extend(new_stimuli['vert_sweep'])
+    # extend is always in place, so this will work, but it just will be in horiz_sweep, which is a
+    # little janky
+    sp.io.savemat(save_template_string.format("sweep"),
+                  {'images': np.asarray(new_stimuli['horiz_sweep'])})
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=("Create the stimuli necessary to test the pRF "
+                                                  "location and size."))
+    parser.add_argument("stimuli_path", help=("string. path to the stimuli to modify. Should be "
+                                              "the stimuli.mat from Kendrick's website"))
+    parser.add_argument("save_template_string", help=("string containing one {}. The string that "
+                                                      "we'll save our stimuli at, using .format"))
+    args = parser.parse_args()
+    create_stimuli(args.stimuli_path, args.save_template_string)
